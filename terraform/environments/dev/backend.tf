af94444b4b975d1ff8b67cee6cb71c4cd377d1cc
aidan <aidaneletona@gmail.com>
2026-09-14 15:44:25 -0700

    reconfigured backends

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-oidc-state-aidan"
-    key     = "prod/terraform.tfstate"
-    region  = "us-east-2"
-    encrypt = true
+    bucket       = "terraform-oidc-state-aidan"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
