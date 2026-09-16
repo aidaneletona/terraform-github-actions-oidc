c1b3cd5446b79c769bf483ff3cd0eea80e6f48f6
aidan <aidaneletona@gmail.com>
2026-09-15 21:39:03 -0700

    Restore secure subnet configuration

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-2a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name        = "${var.project}-${var.environment}-public-subnet"
