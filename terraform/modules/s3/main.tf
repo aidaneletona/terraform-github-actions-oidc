f618d88dbb9976304cd7544493e527fab4cfe4ec
aidan <aidaneletona@gmail.com>
2026-09-19 19:27:57 -0700

    Promo prod test done

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -59,7 +59,6 @@ resource "aws_s3_bucket" "demo" {
     Project     = var.project
     Environment = var.environment
     Purpose     = var.demo_purpose
-    PromotionTest = "shared-module"
   }
 }
 
